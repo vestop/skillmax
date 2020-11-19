ad9206a0b9e5416caedd05fb4c6aa0df9860f49b
JaVoHiR <joemvrk@gmail.com>
2020-11-19 21:17:30 +0500

    any fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector('.menu-nav__btn');
 const menu = document.querySelector('.main-nav__dropdown');
 const preload = document.querySelector('.preload');
+const nav = document.querySelector('.page-header__top');
 
 window.addEventListener("load", () => {
   setTimeout((function () {
@@ -13,5 +14,18 @@ window.addEventListener("load", () => {
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('active');
   menu.classList.toggle('active');
-  document.body.classList.toggle('locked')
+  document.body.classList.toggle('locked');
+
+  if (window.pageYOffset < 120) {
+    nav.classList.toggle('header_tiny');
+  }
+});
+
+window.addEventListener("scroll", () => {
+
+  if (window.pageYOffset > 120) {
+    nav.classList.add('header_tiny');
+  } else {
+    nav.classList.remove('header_tiny');
+  }
 });
